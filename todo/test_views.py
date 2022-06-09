e84a4134bed1ab36bb91e0b1df3906ed1648e9a1
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-06-09 23:11:52 +0000

    learning testing in Django

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,8 +1,21 @@
 from django.test import TestCase
+from .models import Item
 
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
+    
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+    def test_get_add_item(self):
+        response = self.client.get('/add_item')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
 
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='test todo item')
+        response = self.client.get('/edit/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
